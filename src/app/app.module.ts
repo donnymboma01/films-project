5368c4a44a9dc79855f489a81447283d531bc3f8
donnymboma01 <donnymboma01@gmail.com>
2022-09-15 02:41:05 +0200

    login & logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MovieComponent } from './movie/movie.component';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   login(username : string, password : string){
     if(username === 'donny_dsm' && password === 'donny' ){
       return 200;
     }else{
-      return 403;
+      return 403; 
     }
+  }
+
+  logout(){
+
   }
 }
